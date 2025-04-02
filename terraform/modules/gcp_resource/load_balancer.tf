4e377685888f2f1ac0c6dc58ff441f22eb7c0e89
lendly-telus-com <lendly.cagata@telus.com>
2025-04-03 01:56:07 +0800

    fix: load gimo

--- terraform/modules/gcp_resource/load_balancer.tf
+++ terraform/modules/gcp_resource/load_balancer.tf
@@ -1,20 +1,11 @@
-resource "google_compute_address" "service_ip" {
-  name         = "my-service-ip"
-  region       = "northamerica-northeast1"
-  address_type = "EXTERNAL"
-}
-
 resource "kubernetes_service" "my_service" {
   metadata {
     name = "my-service"
-    annotations = {
-      "networking.gke.io/load-balancer-type" = "External"
-    }
   }
 
   spec {
     selector = {
-      app = "java-rest-app"  # This should match your deployment's label
+      app = "java-rest-app"
     }
 
     port {
@@ -22,12 +13,6 @@ resource "kubernetes_service" "my_service" {
       target_port = 8080
     }
 
-    type                    = "LoadBalancer"
-    load_balancer_ip        = google_compute_address.service_ip.address
-    external_traffic_policy = "Regional"
+    type = "LoadBalancer"
   }
-}
-
-output "service_ip" {
-  value = google_compute_address.service_ip.address
 }
