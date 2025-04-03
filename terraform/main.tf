7410258392d592436bcab8fd71e9ba60a2d46565
lendly-telus-com <lendly.cagata@telus.com>
2025-04-04 00:04:54 +0800

    fix: gcp resource

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,4 +2,6 @@ module "gcp_resource" {
   source     = "./modules/gcp_resource"
   project_id = var.project_id
   region     = var.region
+  repo_name  = var.repo_name
+  image_tag  = var.image_tag
 }
