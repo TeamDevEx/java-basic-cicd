524f4a20b48373fcfca9ff7b52478dd058eaf98d
lendly-telus-com <lendly.cagata@telus.com>
2025-04-04 00:01:39 +0800

    feat: test 1

--- terraform/modules/gcp_resource/cluster.tf
+++ terraform/modules/gcp_resource/cluster.tf
@@ -1,1 +1,6 @@
-
+resource "google_container_cluster" "my_cluster" {
+  name               = "java-rest-app"
+  location           = var.region
+  enable_autopilot   = true
+  deletion_protection = false
+}
