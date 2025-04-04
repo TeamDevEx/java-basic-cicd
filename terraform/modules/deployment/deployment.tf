803b7f5087035f19a710291b5e79877033d5d209
lendly-telus-com <lendly.cagata@telus.com>
2025-04-05 01:57:02 +0800

    feat: demo dep

---
+++ terraform/modules/deployment/deployment.tf
@@ -0,0 +1,42 @@
+data "google_container_cluster" "my_cluster" {
+  name     = "java-rest-app"
+  location = var.region
+  project  = var.project_id
+}
+
+data "google_client_config" "provider" {}
+
+resource "kubernetes_deployment" "deployment" {
+  metadata {
+    name      = "java-rest-app"
+    namespace = "default"
+    labels = {
+      app = "java-rest-app"
+    }
+  }
+
+  spec {
+    replicas = 3
+
+    selector {
+      match_labels = {
+        app = "java-rest-app"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "java-rest-app"
+        }
+      }
+
+      spec {
+        container {
+          name  = "spring-boot-rest-app-1"
+          image = "northamerica-northeast1-docker.pkg.dev/off-net-dev/lendly-demo/spring-boot-rest-app:latest"
+        }
+      }
+    }
+  }
+}
--- terraform/modules/gcp_resource/deployment.tf
+++
@@ -1,8 +0,0 @@
-data "google_container_cluster" "my_cluster" {
-  name     = "java-rest-app"
-  location = var.region
-  project  = var.project_id
-}
-
-data "google_client_config" "provider" {}
-
