7f8701f845d55425c7029858f48daf20587c9fbe
lendly-telus-com <lendly.cagata@telus.com>
2025-04-06 03:35:18 +0800

    fix: lb

--- k8s/modules/deployment/deployment.tf
+++ k8s/modules/deployment/deployment.tf
@@ -48,3 +48,22 @@ resource "kubernetes_deployment" "deployment" {
   }
 }
 
+resource "kubernetes_service" "my_service" {
+  metadata {
+    name = "my-service"
+  }
+
+  spec {
+    selector = {
+      app = "java-rest-app"
+    }
+
+    port {
+      port        = 80
+      target_port = 8080
+    }
+
+    type = "LoadBalancer"
+  }
+}
+
