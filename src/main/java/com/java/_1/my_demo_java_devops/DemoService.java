186c57b02017e75d6f70fe7bf4585b617b4298ff
lendly-telus-com <lendly.cagata@telus.com>
2025-04-08 02:25:57 +0800

    fix: new join xx

--- src/main/java/com/java/_1/my_demo_java_devops/DemoService.java
+++ src/main/java/com/java/_1/my_demo_java_devops/DemoService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 public class DemoService {
 
 	public String Hello() {
-		return "Hello Jimbit Malupit YOyoyo!! , The logic dreamer!!!!";
+		return "Hello Jimbit Malupit Lala!! , The logic dreamer!!!!";
 
 	}
 
