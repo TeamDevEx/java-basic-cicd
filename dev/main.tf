5e334356c213d32d7f13a4b08170d57bcdc12779
lendly-telus-com <lendly.cagata@telus.com>
2025-04-07 23:35:50 +0800

    feat: update console log xx

--- dev/main.tf
+++ dev/main.tf
@@ -1,8 +1,11 @@
 module "gcp_resource" {
-  source     = "./modules/gcp_resource"
-  project_id = var.project_id
-  region     = var.region
-  repo_name  = var.repo_name
-  image_tag  = var.image_tag
+  source = "./modules/gcp_resource"
+
   environment = var.environment
+  project_id  = var.project_id
+  region      = var.region
+  repo_name   = var.repo_name
+  image_tag   = var.image_tag
 }
+
+
