221d7340007135738a159e0d68d9a63a53f2cb31
lendly-telus-com <lendly.cagata@telus.com>
2025-04-08 00:01:41 +0800

    fix: dep

--- dev/modules/gcp_resource/deployment.tf
+++ dev/modules/gcp_resource/deployment.tf
@@ -6,47 +6,7 @@ data "google_container_cluster" "my_cluster" {
 
 data "google_client_config" "provider" {}
 
-resource "kubernetes_namespace" "env" {
-  metadata {
-    name = "namespace-${var.environment}"  
-  }
-}
 
-
-resource "kubernetes_deployment" "deployment" {
-  metadata {
-    name      = "java-rest-app"
-    namespace = kubernetes_namespace.env.metadata[0].name
-    labels = {
-      app = "java-rest-app"
-    }
-  }
-
-  spec {
-    replicas = 3
-
-    selector {
-      match_labels = {
-        app = "java-rest-app"
-      }
-    }
-
-    template {
-      metadata {
-        labels = {
-          app = "java-rest-app"
-        }
-      }
-
-      spec {
-        container {
-          name  = "spring-boot-rest-app-1"
-          image = "northamerica-northeast1-docker.pkg.dev/${var.project_id}/${var.repo_name}/spring-boot-rest-app:${var.image_tag}"
-        }
-      }
-    }
-  }
-}
 
 
 
