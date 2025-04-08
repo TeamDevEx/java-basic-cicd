cfd15b1128f2660b001827b9c5ff3e321346f6df
lendly-telus-com <lendly.cagata@telus.com>
2025-04-08 23:48:20 +0800

    fix: round2 xx

--- dev/modules/gcp_resource/deployment.tf
+++ dev/modules/gcp_resource/deployment.tf
@@ -6,5 +6,74 @@ data "google_container_cluster" "my_cluster" {
 
 data "google_client_config" "provider" {}
 
+resource "kubernetes_deployment" "deployment-2" {
+  metadata {
+    name      = "java-rest-app-dev-2"
+    namespace = kubernetes_namespace.env.metadata[0].name
+  }
+
+  spec {
+    replicas = 5 
+
+    selector {
+      match_labels = {
+        app = "java-rest-app-dev-2"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "java-rest-app-dev-2"
+        }
+      }
+
+      spec {
+        container {
+          name  = "spring-boot-rest-app"
+          image = "northamerica-northeast1-docker.pkg.dev/${var.project_id}/${var.repo_name}/spring-boot-rest-app:${var.image_tag}"
+
+          resources {
+            requests = {
+              cpu    = "500m"
+              memory = "256Mi"
+            }
+            limits = {
+              cpu    = "1"
+              memory = "512Mi"
+            }
+          }
+
+          readiness_probe {
+            http_get {
+              path = "/healthz"
+              port = 8080
+            }
+            initial_delay_seconds = 10
+            period_seconds        = 5
+          }
+
+          liveness_probe {
+            http_get {
+              path = "/healthz"
+              port = 8080
+            }
+            initial_delay_seconds = 30
+            period_seconds        = 10
+          }
+        }
+      }
+    }
+
+    strategy {
+      type = "RollingUpdate"
+
+      rolling_update {
+        max_surge       = "25%"
+        max_unavailable = "0" # Ensures zero downtime during updates.
+      }
+    }
+  }
+}
 
 
