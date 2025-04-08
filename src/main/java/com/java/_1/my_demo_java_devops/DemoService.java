caa2f904861b1afcd23c1b360d26054976209e43
lendly-telus-com <lendly.cagata@telus.com>
2025-04-09 00:53:02 +0800

    fix: roll 3 xxx

--- src/main/java/com/java/_1/my_demo_java_devops/DemoService.java
+++ src/main/java/com/java/_1/my_demo_java_devops/DemoService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 public class DemoService {
 
 	public String Hello() {
-		return "Hello Pogs!! , The logic dreamer!!!!";
+		return "Hello Pogs!!** , The logic dreamer!!!!";
 
 	}
 
